5f25fe2808a420bc1ee7f5e15fcddf97d5165daa
Emre <sahinemre.dev@gmail.com>
2023-06-14 03:45:08 +0300

    Add game function

--- main.js
+++ main.js
@@ -10,11 +10,20 @@ function playRound(playerChoice,computerChoice){
     else if (computerChoice=="Rock" && playerChoice=="Paper"){winCheck=1;}
     else if (computerChoice==playerChoice) {return 'It\'s a tie!';}
 
-    if (winCheck==1) return (`${playerChoice} beats ${computerChoice}. You win!`);
-    else return (`${computerChoice} beats ${playerChoice}. You lose!`);
+    if (winCheck==1){playerWin++; return (`${playerChoice} beats ${computerChoice}. You win!`);}
+    else {computerWin++; return (`${computerChoice} beats ${playerChoice}. You lose!`);}
+}
+function game(){
+    for (let i=0;i<5;i++){
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+    }
 }
 
+let playerWin=0;
+let computerWin=0
+const playerSelection = prompt("What will you choose?");
+game();
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+playerWin>computerWin ? console.log("You win!"): console.log("You lose!");
+console.log(playerWin + " matches won.");
