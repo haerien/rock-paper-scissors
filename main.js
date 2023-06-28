fadb33db59e48c79e1d28cdbfbe9c1049c7494ac
Emre <sahinemre.dev@gmail.com>
2023-06-28 08:38:36 +0300

    Improve UI

--- main.js
+++ main.js
@@ -7,21 +7,26 @@ function getPlayerChoice(choice){
     return choice.innerText;
 }
 
-const playerScore = document.querySelector(".player-score");
-const computerScore = document.querySelector(".computer-score");
+function showChoices(computerChoice,playerChoice){
+    choices.innerHTML = `Computer picked <span style="color: red">${computerChoice}.</span> Player picked <span style="color: red">${playerChoice}. </span>`;
+}
+
+const playerScore = document.querySelector(".player.score");
+const computerScore = document.querySelector(".computer.score");
 const result = document.querySelector("#result");
 const choices = document.querySelector("#choices");
 
 function playRound(computerChoice, playerChoice){
     if(playerScore.innerText!="5" && computerScore.innerText!="5"){
         let winCheck=0;
+        showChoices(computerChoice,playerChoice);
         if (computerChoice=="Scissors" && playerChoice=="Rock") winCheck=1;
         else if (computerChoice=="Paper" && playerChoice=="Scissors") winCheck=1;
         else if (computerChoice=="Rock" && playerChoice=="Paper") winCheck=1;
         else if (computerChoice==playerChoice) result.innerText = 'It\'s a tie!';
-
-        if (winCheck==1){playerScore.innerText++; result.innerText = (`${playerChoice} beats ${computerChoice}. You win!`);}
-        else {computerScore.innerText++; result.innerText = (`${computerChoice} beats ${playerChoice}. You lose!`);}
+        else {computerScore.innerText++; result.innerText = `${computerChoice} beats ${playerChoice}. You lose!`;}
+        
+        if (winCheck==1){playerScore.innerText++; result.innerText = `${playerChoice} beats ${computerChoice}. You win!`;}
     
         if(playerScore.innerText=="5" || computerScore.innerText==5){
             if (computerScore.innerText > playerScore.innerText) result.innerText="Computer won!";
